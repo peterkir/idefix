064460e72c0dd383aa512fd50fb4e6a2b8f61b31
Peter Kirschner <peter@kirschners.de>
2016-03-01 14:34:01 +0100

    re-structuring bootstrap

--- bootstrap/peterkir/win32/engageOriginal.cmd
+++ bootstrap/peterkir/win32/engageOriginal.cmd
@@ -16,12 +16,12 @@ IF "%1"=="" (
 )
 
 IF "%2"=="" (
-	SET JAVA_WEB=http://www.klib.io/_archives/java/1.8/
+	SET JAVA_WEB=http://www.klib.io/_archives/java/1.8
 ) ELSE (
 	::SET JAVA_WEB=https://s3-eu-west-1.amazonaws.com/klib.io/www/_archives/java/zipped/1.8
 	SET JAVA_WEB=http://jazz01.rd.corpintra.net/web/repo/_archives/java/zipped/1.8
 )
-SET JAVA_ARCHIVE=win32.x86_64-jre1.8.0_60.zip
+SET JAVA_ARCHIVE=win32.x86_64-jre1.8.0_74.zip
 SET JAVA=%JAVA_ARCHIVE:~13,-4%
 
 :: ###########################################################
@@ -32,9 +32,11 @@ SET SCRIPT_PATH=%SCRIPT_PATH:~0,-1%
 SET OOMPH_NAME=oomphInstaller
 SET DOWNLOAD_LOCATION=%SCRIPT_PATH%\download
 
-SET BRANCH=peterkir
-SET GITHUB_IO=http://peterkir.github.io/org.eclipse.oomph/
-SET GITHUB_IO_LATEST=latest
+SET ECLIPSE_FILEDOWNLOAD_URL=http://www.eclipse.org/downloads/download.php
+SET ECLIPSE_INSTALLER_EXE=eclipse-inst-win64.exe
+SET FILEPATH=/oomph/products/%ECLIPSE_INSTALLER_EXE%
+:: download from best mirror [ &r=1 ]
+SET "DOWNLOADURL=%ECLIPSE_FILEDOWNLOAD_URL%?file=%FILEPATH%&r=1"
 
 ::ECHO.
 ::ECHO # workaroung for NTLMv2 ProxyAuth - starting IE
@@ -56,28 +58,15 @@ ECHO.
 MKDIR %SCRIPT_PATH%\download 2>&1 > NUL
 
 :: download of a file with powershell - http://superuser.com/a/423789/344922
-ECHO retrieving latest build 
-powershell -nologo -noprofile -command "%POWERSHELL_TITLE%; (New-Object System.Net.WebClient).DownloadFile('%GITHUB_IO%/%BRANCH%/%GITHUB_IO_LATEST%','%DOWNLOAD_LOCATION%\%BRANCH%_%GITHUB_IO_LATEST%')"
-SET /P LATEST=<%DOWNLOAD_LOCATION%\%BRANCH%_%GITHUB_IO_LATEST%
-
-SET BINTRAY_BUILD_ROOT=https://dl.bintray.com/peterkir/generic/org.eclipse.oomph/1.3.0/%BRANCH%/%LATEST%
-
-SET ECLIPSE_INSTALLER_WEB=%BINTRAY_BUILD_ROOT%/products
-SET ECLIPSE_INSTALLER_ARCHIVE=org.eclipse.oomph.setup.installer.product-win32.win32.x86_64.zip
-SET ECLIPSE_INSTALLER=%ECLIPSE_INSTALLER_ARCHIVE:~0,-4%
-
-ECHO downloading archive %ECLIPSE_INSTALLER_WEB%/%ECLIPSE_INSTALLER_ARCHIVE%
-powershell -nologo -noprofile -command "%POWERSHELL_TITLE%;if ( Test-Path %DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_ARCHIVE% ) { Write-Output 'skipping download, cause file exists - %DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_ARCHIVE%' } else {(New-Object System.Net.WebClient).DownloadFile('%ECLIPSE_INSTALLER_WEB%/%ECLIPSE_INSTALLER_ARCHIVE%','%DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_ARCHIVE%')}"
+ECHO "downloading archive %DOWNLOADURL%"
+powershell -nologo -noprofile -command "%POWERSHELL_TITLE%;if ( Test-Path %DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_EXE% ) { Write-Output 'skipping download, cause file exists - %DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_EXE%' } else {$wc=(New-Object System.Net.WebClient);$wc.Headers['User-Agent']='Safari';$wc.DownloadFile('%DOWNLOADURL%','%DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_EXE%')}"
 IF "%ERRORLEVEL%"=="" (
 	ECHO failing downloading file %ECLIPSE_INSTALLER_WEB%/%ECLIPSE_INSTALLER_ARCHIVE%
 	GOTO END
 )
 
-ECHO extracting ECLIPSE_INSTALLER archive to %ECLIPSE_INSTALLER%
-powershell -nologo -noprofile  -command "%POWERSHELL_TITLE%;if ( Test-Path '%SCRIPT_PATH%\%OOMPH_NAME%' -PathType Container )  { Write-Output 'skipping extraction, cause folder exists - %SCRIPT_PATH%\%OOMPH_NAME%' } else {Add-Type -A System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory('%DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_ARCHIVE%', '%SCRIPT_PATH%\%OOMPH_NAME%')}"
-
 ECHO.
-ECHO # downloading and configuring %JAVA_ARCHIVE%
+ECHO # downloading and configuring Java - %JAVA%
 ECHO.
 ECHO downloading archive %JAVA_WEB%/%JAVA_ARCHIVE%
 powershell -nologo -noprofile  -command "%POWERSHELL_TITLE%;if ( Test-Path %DOWNLOAD_LOCATION%\%JAVA_ARCHIVE% ) { Write-Output 'skipping download, cause file exists - %DOWNLOAD_LOCATION%\%JAVA_ARCHIVE%' } else {(New-Object System.Net.WebClient).DownloadFile('%JAVA_WEB%/%JAVA_ARCHIVE%','%DOWNLOAD_LOCATION%\%JAVA_ARCHIVE%')}"
@@ -86,61 +75,31 @@ IF "%ERRORLEVEL%"=="" (
 	GOTO END
 )
 
-ECHO extracting %ECLIPSE_INSTALLER% archive to %ECLIPSE_INSTALLER%/%JAVA%
-powershell -nologo -noprofile  -command "%POWERSHELL_TITLE%;if ( Test-Path '%SCRIPT_PATH%\%OOMPH_NAME%\jre' -PathType Container )  { Write-Output 'skipping extraction, cause folder exists - %SCRIPT_PATH%\%OOMPH_NAME%\jre' } else {Add-Type -A System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory('%DOWNLOAD_LOCATION%\%JAVA_ARCHIVE%', '%SCRIPT_PATH%\download');Move-Item %DOWNLOAD_LOCATION%\%JAVA% %SCRIPT_PATH%\%OOMPH_NAME%\jre}"
+ECHO extracting %JAVA_ARCHIVE% archive to %SCRIPT_PATH%/%JAVA%
+powershell -nologo -noprofile  -command "%POWERSHELL_TITLE%;if ( Test-Path '%SCRIPT_PATH%\%JAVA%' -PathType Container )  { Write-Output 'skipping extraction, cause folder exists - %SCRIPT_PATH%\%JAVA%' } else {Add-Type -A System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory('%DOWNLOAD_LOCATION%\%JAVA_ARCHIVE%', '%SCRIPT_PATH%')}"
 
-SET JAVA_HOME=%SCRIPT_PATH%\%OOMPH_NAME%\jre
-SET "OOMPH_HOME=%SCRIPT_PATH%\oomph"
-SET "P2_HOME=%SCRIPT_PATH%\p2"
-
-MKDIR %OOMPH_HOME% 2>&1 > NUL
-SET OOMPH_HOME=%OOMPH_HOME:\=/%
-SET OOMPH_INI=%SCRIPT_PATH%\%OOMPH_NAME%\eclipse-inst.ini
-
-ECHO adding vmArgs to ini file
-
-:: custom added parameters
-ECHO -Doomph.home=%OOMPH_HOME% >> %OOMPH_INI%
-ECHO -Doomph.p2.agent.path=%P2_HOME% >> %OOMPH_INI%
-
-::SET INSTALL_ROOT=%SCRIPT_PATH%\install
-::MKDIR %INSTALL_ROOT% 2>&1 > NUL
-::SET INSTALL_ROOT=%INSTALL_ROOT:\=/%
-::ECHO -Dinstall.root=%INSTALL_ROOT% >> %OOMPH_INI%
-
-::SET WRKSPC_ROOT=%SCRIPT_PATH%\wrkspc
-::MKDIR %WRKSPC_ROOT% 2>&1 > NUL
-::SET WRKSPC_ROOT=%WRKSPC_ROOT:\=/%
-::ECHO -Dworkspace.container.root=%WRKSPC_ROOT% >> %OOMPH_INI%
+SET JAVA_HOME=%SCRIPT_PATH%\%JAVA%
 
 :: allow installation of unsigned bundles
-ECHO -Declipse.p2.unsignedPolicy=allow >> %OOMPH_INI%
-
-:: hidden p2 options (configured to default)
-ECHO -Declipse.p2.max.threads=4  >> %OOMPH_INI%
-ECHO -Declipse.p2.force.threading=true >> %OOMPH_INI%
-ECHO -Declipse.p2.mirrors=true >> %OOMPH_INI%
+SET "VMARGS=-Declipse.p2.unsignedPolicy=allow -Declipse.p2.max.threads=4 -Declipse.p2.force.threading=true -Declipse.p2.mirrors=true -Doomph.setup.installer.mode=ADVANCED -Doomph.setup.jre.choice=false -Doomph.installer.update.url=http://download.eclipse.org/oomph/products/latest/repository -Doomph.update.url=http://download.eclipse.org/oomph/updates/latest -Doomph.redirection.klibProductCatalog=index:/redirectable.products.setup-^>http://peterkir.github.io/idefix/oomph/peterkir/products/productsCatalog.setup -Doomph.redirection.klibProjectCatalog=index:/redirectable.projects.setup-^>http://peterkir.github.io/idefix/oomph/peterkir/projects/projectsCatalog.setup"
 
 :: filtering user displayed catalogs/products/versions
-::ECHO -Doomph.setup.product.catalog.filter=io.klib.products >> %OOMPH_INI%
-::ECHO -Doomph.setup.product.filter=idefix.cec.161 >> %OOMPH_INI%
-::ECHO -Doomph.setup.product.version.filter=none >> %OOMPH_INI%
+::ECHO -Doomph.setup.product.catalog.filter=io.klib.products ^
+::ECHO -Doomph.setup.product.filter=idefix.cec.161 ^
+::ECHO -Doomph.setup.product.version.filter=none ^
 
-ECHO -Doomph.setup.jre.choice=false >> %OOMPH_INI%
-ECHO -Doomph.installer.update.url=%BINTRAY_BUILD_ROOT%/p2/installer >> %OOMPH_INI%
-ECHO -Doomph.update.url=%BINTRAY_BUILD_ROOT%/p2/oomph >> %OOMPH_INI%
-ECHO -Doomph.setup.installer.mode=advanced >> %OOMPH_INI%
-ECHO -Doomph.redirection.klibProductCatalog=index:/redirectable.products.setup-^>http://peterkir.github.io/idefix/oomph/products/productsCatalog.setup >> %OOMPH_INI%
-ECHO -Doomph.redirection.klibProjectCatalog=index:/redirectable.projects.setup-^>http://peterkir.github.io/idefix/oomph/projects/projectsCatalog.setup >> %OOMPH_INI%
 
 ECHO.
 ECHO # launching %OOMPH_NAME%
 ECHO.
-START /B %SCRIPT_PATH%\%OOMPH_NAME%\eclipse-inst.exe
 
-ECHO.
-ECHO # clean-up
-ECHO.
-RMDIR /Q /S %DOWNLOAD_LOCATION%
+ECHO %DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_EXE% ^^
+ECHO -vm %SCRIPT_PATH%\%JAVA% ^^
+ECHO -vmargs %VMARGS%
+
+%DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_EXE% ^
+-vm %SCRIPT_PATH%\%JAVA% ^
+-vmargs %VMARGS%
+
 
 :END
