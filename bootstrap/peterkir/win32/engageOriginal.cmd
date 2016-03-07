9a8cae68d6fdfe06f93182c9ed0d6ea3f4dff842
Peter Kirschner <peter@kirschners.de>
2016-03-07 09:34:42 +0100

    rework setup models

--- bootstrap/peterkir/win32/engageOriginal.cmd
+++ bootstrap/peterkir/win32/engageOriginal.cmd
@@ -5,7 +5,7 @@ SETLOCAL EnableDelayedExpansion
 :: ### Version History -- X.Y.Z_YYYYMMDD-hhmm Author Description
 SET version=1.0.0_20150930-0600 &: Peter Kirschner   downloading,extract,configure and launch eclipse-installer with jre
 SET version=%version: =%
-SET title=oomphInstaller script %~nx0 - version %version%
+SET title=idefixInstaller script %~nx0 - version %version%
 TITLE %title%
 SET POWERSHELL_TITLE=$Host.UI.RawUI.WindowTitle='%title%'
 
@@ -29,7 +29,7 @@ SET SCRIPTNAME=%~n0
 SET SCRIPT_PATH=%~dp0
 SET SCRIPT_PATH=%SCRIPT_PATH:~0,-1%
 
-SET OOMPH_NAME=oomphInstaller
+SET IDEFIX_NAME=idefixInstaller
 SET DOWNLOAD_LOCATION=%SCRIPT_PATH%\download
 
 SET BRANCH=master
@@ -74,7 +74,7 @@ IF "%ERRORLEVEL%"=="" (
 )
 
 ECHO extracting ECLIPSE_INSTALLER archive to %ECLIPSE_INSTALLER%
-powershell -nologo -noprofile  -command "%POWERSHELL_TITLE%;if ( Test-Path '%SCRIPT_PATH%\%OOMPH_NAME%' -PathType Container )  { Write-Output 'skipping extraction, cause folder exists - %SCRIPT_PATH%\%OOMPH_NAME%' } else {Add-Type -A System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory('%DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_ARCHIVE%', '%SCRIPT_PATH%\%OOMPH_NAME%')}"
+powershell -nologo -noprofile  -command "%POWERSHELL_TITLE%;if ( Test-Path '%SCRIPT_PATH%\%IDEFIX_NAME%' -PathType Container )  { Write-Output 'skipping extraction, cause folder exists - %SCRIPT_PATH%\%IDEFIX_NAME%' } else {Add-Type -A System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory('%DOWNLOAD_LOCATION%\%ECLIPSE_INSTALLER_ARCHIVE%', '%SCRIPT_PATH%\%IDEFIX_NAME%')}"
 
 ECHO.
 ECHO # downloading and configuring Java - %JAVA%
@@ -87,40 +87,40 @@ IF "%ERRORLEVEL%"=="" (
 )
 
 ECHO extracting %ECLIPSE_INSTALLER% archive to %ECLIPSE_INSTALLER%/%JAVA%
-powershell -nologo -noprofile  -command "%POWERSHELL_TITLE%;if ( Test-Path '%SCRIPT_PATH%\%OOMPH_NAME%\jre' -PathType Container )  { Write-Output 'skipping extraction, cause folder exists - %SCRIPT_PATH%\%OOMPH_NAME%\jre' } else {Add-Type -A System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory('%DOWNLOAD_LOCATION%\%JAVA_ARCHIVE%', '%SCRIPT_PATH%\download');Move-Item %DOWNLOAD_LOCATION%\%JAVA% %SCRIPT_PATH%\%OOMPH_NAME%\jre}"
+powershell -nologo -noprofile  -command "%POWERSHELL_TITLE%;if ( Test-Path '%SCRIPT_PATH%\%IDEFIX_NAME%\jre' -PathType Container )  { Write-Output 'skipping extraction, cause folder exists - %SCRIPT_PATH%\%IDEFIX_NAME%\jre' } else {Add-Type -A System.IO.Compression.FileSystem; [IO.Compression.ZipFile]::ExtractToDirectory('%DOWNLOAD_LOCATION%\%JAVA_ARCHIVE%', '%SCRIPT_PATH%\download');Move-Item %DOWNLOAD_LOCATION%\%JAVA% %SCRIPT_PATH%\%IDEFIX_NAME%\jre}"
 
-SET JAVA_HOME=%SCRIPT_PATH%\%OOMPH_NAME%\jre
-SET "OOMPH_HOME=%SCRIPT_PATH%"
-MKDIR %OOMPH_HOME% 2>&1 > NUL
+SET JAVA_HOME=%SCRIPT_PATH%\%IDEFIX_NAME%\jre
+SET "IDEFIX_HOME=%SCRIPT_PATH%"
+MKDIR %IDEFIX_HOME% 2>&1 > NUL
 
-SET OOMPH_HOME=%OOMPH_HOME:\=/%
-SET OOMPH_INI=%SCRIPT_PATH%\%OOMPH_NAME%\eclipse-inst.ini
+SET IDEFIX_HOME=%IDEFIX_HOME:\=/%
+SET IDEFIX_INI=%SCRIPT_PATH%\%IDEFIX_NAME%\eclipse-inst.ini
 
 ECHO adding vmArgs to ini file
 :: allow installation of unsigned bundles
-ECHO -Declipse.p2.unsignedPolicy=allow >> %OOMPH_INI%
+ECHO -Declipse.p2.unsignedPolicy=allow >> %IDEFIX_INI%
 
 :: hidden p2 options (configured to default)
-ECHO -Declipse.p2.max.threads=4  >> %OOMPH_INI%
-ECHO -Declipse.p2.force.threading=true >> %OOMPH_INI%
-ECHO -Declipse.p2.mirrors=true >> %OOMPH_INI%
+ECHO -Declipse.p2.max.threads=4  >> %IDEFIX_INI%
+ECHO -Declipse.p2.force.threading=true >> %IDEFIX_INI%
+ECHO -Declipse.p2.mirrors=true >> %IDEFIX_INI%
 
 :: filtering user displayed catalogs/products/versions
-ECHO -Doomph.setup.product.catalog.filter=io.klib.products >> %OOMPH_INI%
-::ECHO -Doomph.setup.product.filter=idefix.cec.161 >> %OOMPH_INI%
-::ECHO -Doomph.setup.product.version.filter=none >> %OOMPH_INI%
+ECHO -Doomph.setup.product.catalog.filter=io.klib.products >> %IDEFIX_INI%
+::ECHO -Doomph.setup.product.filter=idefix.cec.161 >> %IDEFIX_INI%
+::ECHO -Doomph.setup.product.version.filter=none >> %IDEFIX_INI%
 
-ECHO -Doomph.setup.jre.choice=false >> %OOMPH_INI%
-::ECHO -Doomph.installer.update.url=%BINTRAY_BUILD_ROOT%/p2/installer >> %OOMPH_INI%
-::ECHO -Doomph.update.url=%BINTRAY_BUILD_ROOT%/p2/oomph >> %OOMPH_INI%
-ECHO -Doomph.setup.installer.mode=advanced >> %OOMPH_INI%
-ECHO -Doomph.redirection.klibProductCatalog=index:/redirectable.products.setup-^>http://peterkir.github.io/idefix/oomph/peterkir/products/productsCatalog.setup >> %OOMPH_INI%
-ECHO -Doomph.redirection.klibProjectCatalog=index:/redirectable.projects.setup-^>http://peterkir.github.io/idefix/oomph/peterkir/projects/projectsCatalog.setup >> %OOMPH_INI%
+ECHO -Doomph.setup.jre.choice=false >> %IDEFIX_INI%
+::ECHO -Doomph.installer.update.url=%BINTRAY_BUILD_ROOT%/p2/installer >> %IDEFIX_INI%
+::ECHO -Doomph.update.url=%BINTRAY_BUILD_ROOT%/p2/oomph >> %IDEFIX_INI%
+ECHO -Doomph.setup.installer.mode=advanced >> %IDEFIX_INI%
+ECHO -Doomph.redirection.klibProductCatalog=index:/redirectable.products.setup-^>http://peterkir.github.io/idefix/oomph/peterkir/productsCatalog.setup >> %IDEFIX_INI%
+ECHO -Doomph.redirection.klibProjectCatalog=index:/redirectable.projects.setup-^>http://peterkir.github.io/idefix/oomph/peterkir/projectsCatalog.setup >> %IDEFIX_INI%
 
 ECHO.
-ECHO # launching %OOMPH_NAME%
+ECHO # launching %IDEFIX_NAME%
 ECHO.
-START /B %SCRIPT_PATH%\%OOMPH_NAME%\eclipse-inst.exe
+START /B %SCRIPT_PATH%\%IDEFIX_NAME%\eclipse-inst.exe
 
 ::ECHO.
 ::ECHO # clean-up
