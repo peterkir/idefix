ccc18735ab03c17c69f63be0ef429b1d94c644fe
Peter Kirschner <peter@kirschners.de>
2017-07-06 19:20:19 +0200

    OS independent execution

--- io.klib.prepare.eclipseinstaller/src/io/klip/prepare/eclipseinstaller/EclipseInstallerPatchINI.java
+++ io.klib.prepare.eclipseinstaller/src/io/klip/prepare/eclipseinstaller/EclipseInstallerPatchINI.java
@@ -1,18 +1,21 @@
 package io.klip.prepare.eclipseinstaller;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
 import java.nio.channels.ReadableByteChannel;
+import java.nio.file.DirectoryStream;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
@@ -22,12 +25,19 @@ import java.util.Arrays;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
-import java.util.StringJoiner;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 import java.util.stream.Collectors;
 
+import org.apache.commons.compress.archivers.tar.TarArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
+import org.apache.commons.compress.compressors.gzip.GzipCompressorOutputStream;
+import org.apache.commons.compress.utils.IOUtils;
 import org.eclipse.oomph.extractor.lib.BINExtractor;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -45,28 +55,39 @@ public class EclipseInstallerPatchINI {
 
 	private static final String SEP = File.separator;
 
+	// @formatter:off
 	private static final String DL_PROD = "http://www.eclipse.org/downloads/download.php?file=/oomph/products";
 	private static final String DL_NIGHTLY = "http://www.eclipse.org/downloads/download.php?file=/oomph/products/latest";
 
-	//@formatter:off
-	private static final String DIR       = System.getProperty("user.dir");
-	private static final String wrkDir    = DIR + SEP + "_wrk";
-	private static final String resultRootDir = DIR + SEP + "_result" + SEP + "eclipseInstaller_" + LocalDate.now() ; 
+	private static final String ECL_DL_SUFFIX = "&r=1";
 	
+	String[][] archives = {
+		{ "prod.win32.x86-64",    DL_PROD,    "eclipse-inst-win64.exe",    "eclipse-inst-prod.win32.x86-64.exe" },
+		{ "prod.win32.x86",       DL_PROD,    "eclipse-inst-win32.exe",    "eclipse-inst-prod.win32.x86.exe" },
+		{ "nightly.win32.x86-64", DL_NIGHTLY, "eclipse-inst-win64.exe",    "eclipse-inst-nightly.win32.x86-64.exe" },
+		{ "nightly.win32.x86",    DL_NIGHTLY, "eclipse-inst-win32.exe",    "eclipse-inst-nightly.win32.x86.exe" },
+		{ "prod.macosx.x86-64",   DL_PROD,    "eclipse-inst-mac64.tar.gz", "eclipse-inst-prod.macosx.x86-64.tar.gz" }
+	};
+
+	private static final String DIR = System.getProperty("user.dir");
+	private static final String wrkDir = DIR + SEP + "_wrk";
+	private static final String resultRootDir = DIR + SEP + "_result" + SEP + "eclipseInstaller_" + LocalDate.now();
+
+	private static final int BUFFER_SIZE = 4096;
+
 	HashMap<String, String[]> iniSuffix = new LinkedHashMap<String, String[]>();
-	
+
 	public EclipseInstallerPatchINI() {
-		iniSuffix.put("peterkir", new String[]{ 
-			"-Doomph.setup.installer.mode=advanced",
-			"-Doomph.setup.installer.p2pool=C:/oomph/.p2",
-			"-Doomph.setup.installer.launch=true",
+		iniSuffix.put("peterkir", new String[] { "-Doomph.setup.installer.mode=advanced",
+			"-Doomph.setup.installer.p2pool=C:/oomph/.p2", "-Doomph.setup.installer.launch=true",
 			"-Doomph.redirection.idefixProductCatalog=index:/redirectable.products.setup->http://peterkir.github.io/idefix/bootstrap/peterkir/catalogProducts.setup",
 			"-Doomph.redirection.idefixProjectCatalog=index:/redirectable.projects.setup->http://peterkir.github.io/idefix/bootstrap/peterkir/catalogProjects.setup",
 			"-Doomph.setup.product.catalog.filter=io\\\\.klib\\\\.products",
 			"-Doomph.setup.product.filter=(?\\!io\\\\.klib\\\\.products\\\\.idefix\\\\.oxygen).*",
 			"-Doomph.setup.product.version.filter=.*\\\\.latest\\\\.cloudbees"
 		});
-		iniSuffix.put("daimler.cec", new String[]{ 
+		
+		iniSuffix.put("daimler.cec", new String[] {
 			"-Doomph.setup.installer.mode=advanced",
 			"-Doomph.setup.installer.p2pooll=C:/oomph/.p2",
 			"-Doomph.setup.installer.launch=true",
@@ -76,7 +97,8 @@ public class EclipseInstallerPatchINI {
 			"-Doomph.setup.product.filter=(?\\!com\\\\.daimler\\\\.products\\\\.idefix\\\\.daimler).*",
 			"-Doomph.setup.product.version.filter=.*\\\\.developer"
 		});
-		iniSuffix.put("daimler.duke", new String[]{ 
+		
+		iniSuffix.put("daimler.duke", new String[] {
 			"-Doomph.setup.installer.mode=advanced",
 			"-Doomph.setup.installer.p2pool=C:/oomph/.p2",
 			"-Doomph.setup.installer.launch=true",
@@ -87,19 +109,13 @@ public class EclipseInstallerPatchINI {
 			"-Doomph.setup.product.version.filter=.*\\\\.duke"
 		});
 	};
-	
-	String[][] archives = { 
-		{ "prod.win32.x86-64"   , DL_PROD,    "eclipse-inst-win64.exe", "&r=1" , "eclipse-inst-prod.win32.x86-64.exe"},
-		{ "prod.win32.x86"      , DL_PROD,    "eclipse-inst-win32.exe", "&r=1" , "eclipse-inst-prod.win32.x86.exe"}, 
-		{ "nightly.win32.x86-64", DL_NIGHTLY, "eclipse-inst-win64.exe", "&r=1" , "eclipse-inst-nightly.win32.x86-64.exe"},
-		{ "nightly.win32.x86"   , DL_NIGHTLY, "eclipse-inst-win32.exe", "&r=1" , "eclipse-inst-nightly.win32.x86.exe"} 
-	};
-	//@formatter:on
+
+	// @formatter:on
 
 	@Activate
 	public void activate() {
 
-		//@formatter:off
+		// @formatter:off
 		createWorkingDirs();
 
 		for (int archiveIndex = 0; archiveIndex < archives.length; archiveIndex++) {
@@ -107,64 +123,170 @@ public class EclipseInstallerPatchINI {
 			String productVersion     = archives[archiveIndex][0];
 			String sourceUrl          = archives[archiveIndex][1];
 			String archiveName        = archives[archiveIndex][2];
-			String dlSuffix           = archives[archiveIndex][3];
 			String patchedArchiveName = archives[archiveIndex][4];
 
-			String downloadDir   = wrkDir + SEP + productVersion + SEP + "1_download" ;
-			String extractDir    = wrkDir + SEP + productVersion + SEP + "2_extracted" ; 
-			String patchDir      = wrkDir + SEP + productVersion + SEP + "3_patched_INI" ; 
+			String downloadDir = wrkDir + SEP + productVersion + SEP + "1_download";
+			String extractDir  = wrkDir + SEP + productVersion + SEP + "2_extracted";
+			String patchDir    = wrkDir + SEP + productVersion + SEP + "3_patched_INI";
 
 			iniSuffix.forEach((variant, iniSuffix) -> {
-				
-				String resultDir      = resultRootDir + SEP + variant  ; 
+
+				String resultDir = resultRootDir + SEP + variant;
 				System.out.format("# processing variant <%s> product version <%s>\n", variant, productVersion);
 
-				download(sourceUrl, downloadDir, archiveName, dlSuffix);
+				download(sourceUrl, downloadDir, archiveName, ECL_DL_SUFFIX);
 
-				extractExe(downloadDir + SEP + archiveName, extractDir);
-				
-				patchIni(
-					patchDir, 
-					extractDir + SEP + PRODUCT, 
-					ECLIPSE_INST_INI, 
-					iniSuffix
-				);
+				String archiveSuffix = archiveName.replaceFirst(".+?(\\.tar\\.gz|\\.exe)", "$1");
+				switch (archiveSuffix) {
+				case ".exe":
+					extractExe(downloadDir + SEP + archiveName, extractDir);
+					patchIniInZip(patchDir, extractDir + SEP + PRODUCT, ECLIPSE_INST_INI, iniSuffix);
+					replaceWithPatchedINI(extractDir + SEP + PRODUCT, patchDir + SEP + ECLIPSE_INST_INI, SEP + ECLIPSE_INST_INI);
+					packPatchedExe(patchedArchiveName, extractDir, resultDir);
+					break;
 
-				replaceWithPatchedINI(
-					extractDir + SEP + PRODUCT, 
-					patchDir + SEP + ECLIPSE_INST_INI,
-					SEP + ECLIPSE_INST_INI
-				);
+				case ".tar.gz":
+					extractTarGz(downloadDir, archiveName, extractDir);
+					patchIni(
+						patchDir, 
+						extractDir + SEP + "Eclipse Installer.app/Contents/Eclipse/"+ SEP + ECLIPSE_INST_INI, 
+						iniSuffix
+					);
+					packPatchedTarGz(patchedArchiveName, extractDir, resultDir);
+					break;
 
-				packPatchedExe(patchedArchiveName, extractDir, resultDir);
+				default:
+					System.out.format("No strategy for archive <%s> with suffix <%s>", archiveName, archiveSuffix);
+					break;
+				}
+
 			});
-			//@formatter:on
+			// @formatter:on
 		}
 
 		System.out.println("done");
 	}
 
+	private void patchIni(String patchDir, String iniFile, String[] iniSuffixArguments) {
+		try {
+			String suffix = (Arrays.asList(iniSuffixArguments).stream()
+					.collect(Collectors.joining(System.lineSeparator())));
+			Path filePathName = new File(iniFile).toPath();
+			Files.write(filePathName, suffix.getBytes("utf-8"), StandardOpenOption.APPEND);
+			System.out.format("  3. creating patched ini file inside %s \n", filePathName);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void packPatchedTarGz(String patchedArchiveName, String extractDir, String resultDir) {
+		try {
+			Path inputDirectoryPath = new File(extractDir).toPath();
+			File outputFile = new File(resultDir + SEP + patchedArchiveName);
+
+			try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+					BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(fileOutputStream);
+					GzipCompressorOutputStream gzipOutputStream = new GzipCompressorOutputStream(bufferedOutputStream);
+					TarArchiveOutputStream tarArchiveOutputStream = new TarArchiveOutputStream(gzipOutputStream)) {
+
+				tarArchiveOutputStream.setBigNumberMode(TarArchiveOutputStream.BIGNUMBER_POSIX);
+				tarArchiveOutputStream.setLongFileMode(TarArchiveOutputStream.LONGFILE_GNU);
+
+				List<Path> files = listFiles(inputDirectoryPath, new LinkedList<Path>());
+
+				files.forEach(f -> {
+
+					String relativeFilePath = new File(inputDirectoryPath.toUri()).toURI()
+							.relativize(new File(f.toFile().getAbsolutePath()).toURI()).getPath();
+
+					TarArchiveEntry tarEntry = new TarArchiveEntry(f.toFile(), relativeFilePath);
+					tarEntry.setSize(f.toFile().length());
+
+					try {
+						tarArchiveOutputStream.putArchiveEntry(tarEntry);
+						tarArchiveOutputStream.write(IOUtils.toByteArray(new FileInputStream(f.toFile())));
+						tarArchiveOutputStream.closeArchiveEntry();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+
+				);
+				tarArchiveOutputStream.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private List<Path> listFiles(Path path, List<Path> files) throws IOException {
+		try (DirectoryStream<Path> stream = Files.newDirectoryStream(path)) {
+			for (Path entry : stream) {
+				if (Files.isDirectory(entry)) {
+					listFiles(entry, files);
+				} else {
+					files.add(entry);
+				}
+			}
+		}
+		return files;
+	}
+
+	private void extractTarGz(String downloadDir, String archiveName, String extractDir) {
+
+		System.out.format("  2. extracting %s into %s\n", archiveName, extractDir);
+
+		try {
+			File file = new File(downloadDir + SEP + archiveName);
+			GzipCompressorInputStream gzipIn = new GzipCompressorInputStream(new FileInputStream(file));
+			try (TarArchiveInputStream tarIn = new TarArchiveInputStream(gzipIn)) {
+				TarArchiveEntry entry;
+
+				while ((entry = (TarArchiveEntry) tarIn.getNextEntry()) != null) {
+					/** If the entry is a directory, create the directory. **/
+					if (entry.isDirectory()) {
+						File f = new File(extractDir + SEP + entry.getName());
+						boolean created = f.mkdirs();
+						if (!created) {
+							System.out.printf(
+									"Unable to create directory '%s', during extraction of archive contents.\n",
+									f.getAbsolutePath());
+						}
+					} else {
+						int count;
+						byte data[] = new byte[BUFFER_SIZE];
+						FileOutputStream fos = new FileOutputStream(extractDir + SEP + entry.getName(), false);
+						try (BufferedOutputStream dest = new BufferedOutputStream(fos, BUFFER_SIZE)) {
+							while ((count = tarIn.read(data, 0, BUFFER_SIZE)) != -1) {
+								dest.write(data, 0, count);
+							}
+							dest.close();
+						}
+					}
+				}
+				tarIn.close();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	private void packPatchedExe(String archiveName, String extractDir, String resultDir) {
 		System.out.format("  5. packing patched exe %s%s%s \n", resultDir, SEP, archiveName);
-		//@formatter:off
+		// @formatter:off
 		try {
 			Files.createDirectories(Paths.get(resultDir));
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		concatenateFilesOsCall(
-			resultDir + SEP + archiveName, 
-			extractDir,
-			EXTRACTOR_EXE, 
-			MARKER, 
-			EXTRACTOR_LIB, 
-			MARKER, 
-			DESCRIPTOR, 
-			MARKER, 
-			PRODUCT, 
-			MARKER
-		);
-		//@formatter:on
+		// concatenateFilesOsCall(resultDir + SEP + archiveName, extractDir,
+		// EXTRACTOR_EXE, MARKER, EXTRACTOR_LIB, MARKER,
+		// DESCRIPTOR, MARKER, PRODUCT, MARKER);
+
+		concatenateFiles(resultDir + SEP + archiveName, extractDir, EXTRACTOR_EXE, MARKER, EXTRACTOR_LIB, MARKER,
+				DESCRIPTOR, MARKER, PRODUCT, MARKER);
+		// @formatter:on
 	}
 
 	private void replaceWithPatchedINI(String zipArchive, String fileToPack, String filePathInZip) {
@@ -182,7 +304,7 @@ public class EclipseInstallerPatchINI {
 		System.out.format("  4. replacing INI file with patched one inside %s \n", zipArchive);
 	}
 
-	private void patchIni(String extractDir, String jarName, String filename, String[] iniSuffixArguments) {
+	private void patchIniInZip(String extractDir, String jarName, String filename, String[] iniSuffixArguments) {
 		JarFile jar = null;
 		try {
 			jar = new JarFile(jarName);
@@ -226,46 +348,43 @@ public class EclipseInstallerPatchINI {
 
 		System.out.format("  2. extracting %s into %s\n", executable, targetFolder);
 
-		//@formatter:off
-		String markerFile     = MARKER;
-		String productFile    = PRODUCT;
-		String extractorFile  = EXTRACTOR_EXE;
-		String libdataFile    = EXTRACTOR_LIB;
+		// @formatter:off
+		String markerFile = MARKER;
+		String productFile = PRODUCT;
+		String extractorFile = EXTRACTOR_EXE;
+		String libdataFile = EXTRACTOR_LIB;
 		String descriptorFile = DESCRIPTOR;
 
-		String[] args = new String[] { 
-			executable, 
-			targetFolder + SEP + productFile, 
-			"-export", 
-				targetFolder + SEP + markerFile, 
-				targetFolder + SEP + extractorFile, 
-				targetFolder + SEP + libdataFile,
-				targetFolder + SEP + descriptorFile 
-		};
+		String[] args = new String[] { executable, targetFolder + SEP + productFile, "-export",
+				targetFolder + SEP + markerFile, targetFolder + SEP + extractorFile, targetFolder + SEP + libdataFile,
+				targetFolder + SEP + descriptorFile };
 		try {
 			BINExtractor.main(args);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//@formatter:on
+		// @formatter:on
 	}
 
 	private void download(String url, String location, String filename, String suffix) {
 		String prod_win32 = String.format("%s/%s%s", url, filename, suffix);
-
-		System.out.format("  1. downloading %s\n", prod_win32);
-
-		try {
-			Files.createDirectories(Paths.get(location));
-			URL website = new URL(prod_win32);
-			ReadableByteChannel rbc = Channels.newChannel(website.openStream());
-			FileChannel fc = FileChannel.open(Paths.get(location, filename), StandardOpenOption.CREATE,
-					StandardOpenOption.TRUNCATE_EXISTING, StandardOpenOption.WRITE);
-			fc.transferFrom(rbc, 0, Long.MAX_VALUE);
-			rbc.close();
-			fc.close();
-		} catch (Exception e) {
-			e.printStackTrace();
+		Path path = Paths.get(location, filename);
+		if (!path.toFile().exists()) {
+			System.out.format("  1. downloading %s\n", prod_win32);
+			try {
+				Files.createDirectories(Paths.get(location));
+				URL website = new URL(prod_win32);
+				ReadableByteChannel rbc = Channels.newChannel(website.openStream());
+				FileChannel fc = FileChannel.open(path, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING,
+						StandardOpenOption.WRITE);
+				fc.transferFrom(rbc, 0, Long.MAX_VALUE);
+				rbc.close();
+				fc.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else {
+			System.out.format("  1. already existing %s - skipping download\n", prod_win32);
 		}
 	}
 
@@ -277,100 +396,30 @@ public class EclipseInstallerPatchINI {
 		}
 	}
 
-	private void concatenateFilesOsCall(String destFileName, String packDir, String... filesToConcatenate) {
-
-		//@formatter:off
-		StringJoiner sj = new StringJoiner("+");
-		
-		for (String file : filesToConcatenate) {
-			sj.add(file.replaceAll("/", "\\"));
-		}
-		ProcessBuilder pb = new ProcessBuilder(
-			"CMD",
-			"/C",
-			"COPY",
-			"/B",
-			sj.toString(),
-			destFileName
-		);
-		@SuppressWarnings("unused") // necessary to set the env (otherwise it was null)$
-		Map<String, String> env = pb.environment();
-		pb.directory(new File(packDir.replaceAll("/", "\\")));
-		pb.redirectErrorStream(true);
-		try {
-			pb.start();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		//@formatter:on	
-	}
-
 	/*
-	 * concatenation was not working - don't know why - replaced it with windows
-	 * OS system call copy concatenateFilesOsCall
+	 * concatenation was not working - don't know why - replaced it with windows OS
+	 * system call copy concatenateFilesOsCall
 	 */
-	@SuppressWarnings("unused")
-	private void concatenateFiles(String destFileName, String... filesToConcatenate) {
+	private void concatenateFiles(String destFileName, String packDir, String... filesToConcatenate) {
 		System.out.format("concatenation into file %s\n", destFileName);
 
-		// try {
-		// FileChannel dest = new FileOutputStream(destFile).getChannel();
-		// for (String file : filesToConcatenate) {
-		// FileChannel src = new FileInputStream(file).getChannel();
-		// dest.transferFrom(src, 0, src.size());
-		// src.close();
-		// }
-		// dest.close();
-		// } catch (IOException e) {
-		// e.printStackTrace();
-		// }
-
+		OutputStream out;
 		try {
-			//@formatter:off
+			out = new FileOutputStream(destFileName);
+			byte[] buf = new byte[4096];
 			for (String file : filesToConcatenate) {
-				
-				Path destPath = new File(destFileName).toPath();
-				FileChannel fc;
-				if (Files.exists(destPath, LinkOption.NOFOLLOW_LINKS)) {
-					fc = FileChannel.open(
-						destPath,
-						StandardOpenOption.WRITE,
-						StandardOpenOption.APPEND
-					);
-				} else {
-					fc = FileChannel.open(
-						destPath, 
-						StandardOpenOption.CREATE,
-						StandardOpenOption.WRITE
-					);
+				InputStream in = new FileInputStream(packDir + SEP + file);
+				int b = 0;
+				while ((b = in.read(buf)) >= 0) {
+					out.write(buf, 0, b);
+					out.flush();
 				}
-				ReadableByteChannel rbc = Channels.newChannel(new File(file).toURI().toURL().openStream());
-				fc.transferFrom(rbc, 0, Long.MAX_VALUE);
-				fc.close();
-				rbc.close();
+				in.close();
 			}
-			//@formatter:on
+			out.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) {
-		//@formatter:off
-//		String archiveName = "eclipse-inst-win64.exe";
-//		String extractDir = extractRootDir + SEP + 0 + SEP + archiveName;
-//		new EclipseInstallerPatchINI().concatenateFiles(
-//				extractRootDir + SEP + archiveName,
-//				extractDir + SEP + EXTRACTOR_EXE, 
-//				extractDir + SEP + MARKER, 
-//				extractDir + SEP + EXTRACTOR_LIB,
-//				extractDir + SEP + MARKER, 
-//				extractDir + SEP + DESCRIPTOR, 
-//				extractDir + SEP + MARKER,
-//				extractDir + SEP + PRODUCT, 
-//				extractDir + SEP + MARKER
-//		);
-		//@formatter:on
-		System.out.println("done");
-	}
 }
